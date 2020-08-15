698c0dfdb79373a8cb9faeb453758a6b46d0c5dd
Nilesh <nileshjaat341@gmail.com>
2020-08-15 18:08:15 +0530

    finished react side of app

---
+++ public/precache-manifest.b1c71512d501401762b238d9ab792612.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5f3cc257ae4447f9cddf311fd61df46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b83ad66528b500e063e4",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "3298d1e28632c8e7f164",
+    "url": "/static/js/2.ae45a311.chunk.js"
+  },
+  {
+    "revision": "83e895f9a437e69d1a80f15ce6e50621",
+    "url": "/static/js/2.ae45a311.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b83ad66528b500e063e4",
+    "url": "/static/js/main.ed8d1a75.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/static/media/spinner.235b9b3e.gif"
+  }
+]);
